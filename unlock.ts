7b15eddeee89174e26f8f32b0ec15c7804f8effe
ptanh05 <148223045+ptanh05@users.noreply.github.com>
2024-12-31 14:30:48 +0700

    phungtheanh

--- unlock.ts
+++ unlock.ts
@@ -12,16 +12,16 @@ import {
     Constr,
     datumJsonToCbor,
 } from "https://deno.land/x/lucid@0.10.11/mod.ts";
-   
-   import * as cbor from "https://deno.land/x/cbor@v1.4.1/index.js";
-   const lucid = await Lucid.new(
-       new Blockfrost(
-           "https://cardano-preview.blockfrost.io/api/v0",
-           "previewP0pgwulal3HfojJLi0T5p1bhPkFjWuvi"
-       ),
-       "Preview"
-   );
- console.log("20");
+
+import * as cbor from "https://deno.land/x/cbor@v1.4.1/index.js";
+const lucid = await Lucid.new(
+    new Blockfrost(
+        "https://cardano-preview.blockfrost.io/api/v0",
+        "previewP0pgwulal3HfojJLi0T5p1bhPkFjWuvi"
+    ),
+    "Preview"
+);
+console.log("20");
 lucid.selectWalletFromSeed(await Deno.readTextFile("./beneficiary.seed"));
 console.log("22");
 async function readValidator(): Promise<SpendingValidator> {
@@ -36,18 +36,18 @@ const Asset = Data.Object({
     policy_id: Data.Bytes(),    // PolicyId
     token_name: Data.Bytes(),   // AssetName
     amt: Data.Integer(),         // Int
- });
- console.log("39");
- const InheritanceStatus = Data.Enum([
+});
+console.log("39");
+const InheritanceStatus = Data.Enum([
     Data.Literal("Unlocked"),
     Data.Literal("Locked"),
     Data.Literal("Claimed"),
     Data.Literal("Refunded"),
- ]);
+]);
 
- console.log("46");
- const Datum = Data.Object({
-    owner: Data.Bytes(),           
+console.log("46");
+const Datum = Data.Object({
+    owner: Data.Bytes(),
     beneficiary: Data.Bytes(),     // VerificationKeyHash
     lock_until: Data.Integer(),     // Int
     status: InheritanceStatus,   // InheritanceStatus enum
@@ -55,26 +55,26 @@ const Asset = Data.Object({
     nft: Asset,                  // Asset type defined above
     locked_ada: Data.Integer(),     // Int
     plc: Data.Bytes(),            // PolicyId
- });
- const Action = Data.Enum([
+});
+const Action = Data.Enum([
     Data.Literal("Lock"),
     Data.Literal("Refund"),
     Data.Literal("Claim")
-]) ;
- type Action = Data.Static<typeof Action>;
- type Datum = Data.Static<typeof Datum>;
- async function unlock(utxos: UTxO[], currentTime: number, {from, using}: {from: SpendingValidator, using : Redeemer}): Promise<TxHash>{
+]);
+type Action = Data.Static<typeof Action>;
+type Datum = Data.Static<typeof Datum>;
+async function unlock(utxos: UTxO[], currentTime: number, { from, using }: { from: SpendingValidator, using: Redeemer }): Promise<TxHash> {
     const laterTime = new Date(currentTime + 2 * 60 * 60 * 1000).getTime();
 
     console.log("69");
     const tx = await lucid
-    .newTx()
-    .collectFrom(utxos, using)
-    .addSigner(await lucid.wallet.address()) // this should be beneficiary address
-    .validFrom(currentTime)
-    .validTo(laterTime)
-    .attachSpendingValidator(from)
-    .complete();
+        .newTx()
+        .collectFrom(utxos, using)
+        .addSigner(await lucid.wallet.address()) // this should be beneficiary address
+        .validFrom(currentTime)
+        .validTo(laterTime)
+        .attachSpendingValidator(from)
+        .complete();
     console.log("77");
 
     const signedTx = await tx.sign().complete();
@@ -82,33 +82,33 @@ const Asset = Data.Object({
     const txHash = signedTx.submit();
     console.log("Transaction submited oke");
     return txHash;
- }
- async function main(){
+}
+async function main() {
     const beneficiaryPublicKeyHash = lucid.utils.getAddressDetails(await lucid.wallet.address()).paymentCredential?.hash;
     const validator = await readValidator();
     const contractAddress = lucid.utils.validatorToAddress(validator);
     const scriptUTxOs = await lucid.utxosAt(contractAddress);
     const currentTime = new Date().getTime();
-    const utxos = scriptUTxOs.filter((utxo)=> {
+    const utxos = scriptUTxOs.filter((utxo) => {
         try {
             const datum = Data.from<Datum>(utxo.datum ?? '', Datum);
             console.log("Checking UTxO with datum:", datum);
             console.log("Current beneficiary:", beneficiaryPublicKeyHash);
             console.log("Current time:", BigInt(currentTime));
-            
+
             // Kiểm tra từng điều kiện và log ra
             const isBeneficiaryMatch = datum.beneficiary === beneficiaryPublicKeyHash;
             const isLocked = datum.status === "Locked";
             const isTimeExpired = BigInt(datum.lock_until) <= BigInt(currentTime);
             const isPolicyMatch = datum.nft.policy_id === datum.plc;
-             console.log("Conditions:", {
+            console.log("Conditions:", {
                 isBeneficiaryMatch,
                 isLocked,
                 isTimeExpired,
                 isPolicyMatch
             });
-             return isBeneficiaryMatch &&    
-                   isTimeExpired ;
+            return isBeneficiaryMatch &&
+                isTimeExpired;
         } catch (e) {
             console.log(e);
             return false;
@@ -130,6 +130,6 @@ const Asset = Data.Object({
         Redeemer: ${redeemer}
     `);
 
- }
- main();
- //deno run --allow-net --allow-read --allow-env unlock.ts
+}
+main();
+//deno run --allow-net --allow-read --allow-env unlock.ts
